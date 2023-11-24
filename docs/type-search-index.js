8b643f14fc06773cf5df75182fb19192b5ba9948
B3GGS <112033179+B3GGS@users.noreply.github.com>
2023-11-23 23:18:16 -0600

    Adición de JavaDoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"poop12","l":"Cuenta"},{"p":"poop12","l":"HiloCeros"},{"p":"poop12","l":"HiloNones"},{"p":"poop12","l":"HiloR"},{"p":"poop12","l":"HiloRuido"},{"p":"poop12","l":"HilosPares"},{"p":"poop12","l":"HiloT"},{"p":"poop12","l":"HiloUnos"},{"p":"poop12","l":"POOP12"}];updateSearchResults();
